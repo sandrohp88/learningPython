14a9ef1da808280fc324bed3b1f0352614cdf81c
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-02-23 10:52:20 -0800

    list1.py solved...moving to list2.py

--- basic/list1.py
+++ basic/list1.py
@@ -22,7 +22,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+  counter = 0
+  for word in words:
+    if len(word) >=2 and word[0] == word[-1]:
+      counter +=1
+  return counter
 
 
 # B. front_x
@@ -34,7 +38,20 @@ def match_ends(words):
 # before combining them.
 def front_x(words):
   # +++your code here+++
-  return
+  list_x = []
+  # Elements starting with x
+  for word in words:
+    if word.startswith('x'):
+      list_x.append(word)
+
+  # Delete elements starting with x from the list
+  for word in list_x:
+    words.remove(word)
+  
+  # Sort Lists
+  list_x.sort()
+  words.sort()
+  return list_x + words
 
 
 
@@ -56,18 +73,18 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'match_ends'
+  print ('match_ends')
   test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
   test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
   test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
   print
-  print 'front_x'
+  print ('front_x')
   test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
        ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
   test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -77,7 +94,7 @@ def main():
 
        
   print
-  print 'sort_last'
+  print ('sort_last')
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
        [(2, 1), (3, 2), (1, 3)])
   test(sort_last([(2, 3), (1, 2), (3, 1)]),
